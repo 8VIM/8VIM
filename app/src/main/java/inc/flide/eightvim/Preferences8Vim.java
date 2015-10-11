33d75dd0f00292a1d5e13c3d61b934f4ca9d7aaf
flide <flideravi@gmail.com>
2015-10-11 10:31:53 +0530

    Setting up a dummy/Empty Preferences Activity

--- app/src/main/java/inc/flide/eightvim/Preferences8Vim.java
+++ app/src/main/java/inc/flide/eightvim/Preferences8Vim.java
@@ -1,17 +1,36 @@
 package inc.flide.eightvim;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class Preferences8Vim extends PreferenceActivity{
 
     @Override
+    public Intent getIntent() {
+        final Intent modIntent = new Intent(super.getIntent());
+        modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
+        modIntent.putExtra(EXTRA_NO_HEADERS, true);
+        return modIntent;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.activity_preferences_8vim);
-
-        //setContentView(R.layout.activity_preferences_8vim);
+        setTitle(R.string.title_activity_preferences8_vim);
     }
+    @Override
+    protected boolean isValidFragment(final String fragmentName) {
+        return Settings.class.getName().equals(fragmentName);
+    }
+
+    public static class Settings extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.activity_preferences_8vim);
+        }
+    }
+
 }
