69f0bac61d65bad5119b600ee84d28851b71b1cd
Ravi Agarwal <flideravi@gmail.com>
2016-12-31 01:45:16 +0530

    Fix icon file name creation

--- app/src/main/java/inc/flide/eightvim/emojis/adapter/StaticEmojiAdapter.java
+++ app/src/main/java/inc/flide/eightvim/emojis/adapter/StaticEmojiAdapter.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import inc.flide.eightvim.EightVimInputMethodService;
 import inc.flide.eightvim.structures.Emoji;
+import inc.flide.logging.Logger;
 
 public class StaticEmojiAdapter extends BaseEmojiAdapter {
 
@@ -18,10 +19,11 @@ public class StaticEmojiAdapter extends BaseEmojiAdapter {
 
     @Override
     public int getIconId(int position) {
-        String resourceString = filePrefix+emojiList.get(position).getUnicodeHexcode();
+        String resourceString = filePrefix + emojiList.get(position).getUnicodeHexcode().replace('-','_');
         int resourceId;
         resourceId = emojiKeyboardService.getResources().getIdentifier(resourceString, "drawable", emojiKeyboardService.getPackageName());
         if (resourceId == 0) {
+            Logger.d(this, emojiList.get(position).getUnicodeHexcode());
             resourceId = emojiKeyboardService.getResources().getIdentifier("ic_not_available_sign", "drawable", emojiKeyboardService.getPackageName());
         }
         return resourceId;
