ddbe0919333146c4fc82a5282ef18b633a5c8619
Ravi Agarwal <flideravi@gmail.com>
2020-10-16 18:44:11 +0530

    Update version for release

--- 8vim/src/main/java/inc/flide/vim8/structures/Constants.java
+++ 8vim/src/main/java/inc/flide/vim8/structures/Constants.java
@@ -1,7 +1,7 @@
 package inc.flide.vim8.structures;
 
 public interface Constants {
-    String SELF_KEYBOARD_ID = "inc.flide.vim8/.MainInputMethodService";
+    String SELF_KEYBOARD_ID = "inc.flide.vi8/inc.flide.vim8.MainInputMethodService";
     int DELAY_MILLIS_LONG_PRESS_INITIATION = 500;
     int DELAY_MILLIS_LONG_PRESS_CONTINUATION = 50;
     int TEXT_SIZE = 50;
