9bb9bbdeb14ce5224bca14b0eb1437bca99dac24
Noam Postavsky <npostavs@users.sourceforge.net>
2021-05-13 16:46:06 -0400

    Repaint after preference changes
    This means the user doesn't have to close and re-open the keyboard to
    see the effects of each change to circle offset or radius.

--- 8vim/src/main/java/inc/flide/vim8/preferences/SharedPreferenceHelper.java
+++ 8vim/src/main/java/inc/flide/vim8/preferences/SharedPreferenceHelper.java
@@ -5,11 +5,18 @@ import android.content.SharedPreferences;
 
 import androidx.preference.PreferenceManager;
 
-import inc.flide.vim8.R;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class SharedPreferenceHelper {
+public class SharedPreferenceHelper implements SharedPreferences.OnSharedPreferenceChangeListener {
     private SharedPreferences sharedPreferences;
     private static SharedPreferenceHelper singleton = null;
+
+    public static interface Listener {
+        void onPreferenceChanged();
+    }
 
     public static SharedPreferenceHelper getInstance(Context context){
         //These two ifs should be probably swapped as it can still return null
@@ -24,11 +31,28 @@ public class SharedPreferenceHelper {
         return singleton;
     }
 
+    private Set<String> prefKeys = new HashSet<>();
+    private List<Listener> listeners = new ArrayList<>();
+
+    public void addListener(Listener note) {
+        listeners.add(note);
+    }
+
     private SharedPreferenceHelper (SharedPreferences sharedPreferences){
         this.sharedPreferences = sharedPreferences;
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (prefKeys.contains(s)) {
+            for (Listener n : listeners) {
+                n.onPreferenceChanged();
+            }
+        }
     }
 
     public String getString(String preferenceId, String defaultValue) {
+        prefKeys.add(preferenceId);
         String preferencesString;
         try {
             preferencesString = this.sharedPreferences.getString(preferenceId, defaultValue);
@@ -40,6 +64,7 @@ public class SharedPreferenceHelper {
     }
 
     public int getInt(String preferenceId, int defaultValue) {
+        prefKeys.add(preferenceId);
         int preferenceInt;
         try {
             preferenceInt = this.sharedPreferences.getInt(preferenceId, defaultValue);
@@ -51,6 +76,7 @@ public class SharedPreferenceHelper {
     }
 
     public boolean getBoolean(String preferenceId, boolean defaultValue) {
+        prefKeys.add(preferenceId);
         boolean preferenceBoolean;
         try {
             preferenceBoolean = this.sharedPreferences.getBoolean(preferenceId, defaultValue);
@@ -62,6 +88,7 @@ public class SharedPreferenceHelper {
     }
 
     public float getFloat(String preferenceId, float defaultValue) {
+        prefKeys.add(preferenceId);
         float preferenceFloat;
         try {
             preferenceFloat = this.sharedPreferences.getFloat(preferenceId, defaultValue);
