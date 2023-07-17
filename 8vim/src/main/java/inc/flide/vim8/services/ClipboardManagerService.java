2ed153969119fb4b6bd5946604d5ab21636eae58
Ravi Agarwal <flideravi@gmail.com>
2023-07-17 15:25:13 +0530

    📦 feat(ClipboardManagerService.java): create ClipboardManagerService to manage clipboard history
    The MainInputMethodService class now includes an instance of the ClipboardManagerService to handle clipboard functionality. This allows the application to track changes to the clipboard and store a history of clipboard items. The ClipboardManagerService class is responsible for managing the clipboard history by adding new clips to the history and limiting the history size to a maximum of 10 clips. The getClipHistory method allows retrieving the clipboard history.

---
+++ 8vim/src/main/java/inc/flide/vim8/services/ClipboardManagerService.java
@@ -0,0 +1,51 @@
+package inc.flide.vim8.services;
+
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.text.TextUtils;
+import java.util.HashSet;
+import java.util.Set;
+
+import inc.flide.vim8.preferences.SharedPreferenceHelper;
+
+public class ClipboardManagerService {
+
+    private static final String CLIPBOARD_PREFS = "clipboard_prefs";
+    private static final String CLIPBOARD_HISTORY = "clipboard_history";
+    private static final int MAX_HISTORY_SIZE = 10; // This could be made user-configurable
+
+    private final ClipboardManager clipboardManager;
+    private final SharedPreferenceHelper sharedPreferenceHelper;
+
+    public ClipboardManagerService(Context context) {
+        this.sharedPreferenceHelper = SharedPreferenceHelper.getInstance(context);
+        clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+
+        clipboardManager.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
+            @Override
+            public void onPrimaryClipChanged() {
+                String newClip = clipboardManager.getPrimaryClip().getItemAt(0).getText().toString();
+                addClipToHistory(newClip);
+            }
+        });
+    }
+
+    private void addClipToHistory(String newClip) {
+        if (TextUtils.isEmpty(newClip)) return;
+
+        Set<String> history = new HashSet<>(sharedPreferenceHelper.getStringSet(CLIPBOARD_HISTORY, new HashSet<String>()));
+        history.add(newClip);
+
+        // If history size exceeds max, remove oldest clip
+        while (history.size() > MAX_HISTORY_SIZE) {
+            history.remove(history.iterator().next());
+        }
+
+        sharedPreferenceHelper.edit().putStringSet(CLIPBOARD_HISTORY, history).apply();
+    }
+
+    public Set<String> getClipHistory() {
+        return sharedPreferenceHelper.getStringSet(CLIPBOARD_HISTORY, new HashSet<String>());
+    }
+
+}
