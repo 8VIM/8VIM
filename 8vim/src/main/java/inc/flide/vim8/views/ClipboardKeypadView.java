fafad8de18c05bef1bfe4fc2515e0926c8726258
Ravi Agarwal <flideravi@gmail.com>
2023-07-21 17:39:45 +0530

    ✨ feat(ClipboardKeypadView.java): add support for sidebar customization and color theming The superclass of ClipboardKeypadView was changed from ListView to ConstraintLayoutWithSidebar to fix an inheritance issue. This change allows the view to properly inherit the functionality and layout of ConstraintLayoutWithSidebar. Additionally, support for sidebar customization and color theming was added. The view now dynamically inflates the appropriate layout based on the preferred sidebar position and sets the colors of the buttons and background based on the user's color preferences.

--- 8vim/src/main/java/inc/flide/vim8/views/ClipboardKeypadView.java
+++ 8vim/src/main/java/inc/flide/vim8/views/ClipboardKeypadView.java
@@ -2,19 +2,30 @@ package inc.flide.vim8.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import inc.flide.vim8.MainInputMethodService;
+import inc.flide.vim8.R;
 import inc.flide.vim8.geometry.Dimension;
 import inc.flide.vim8.keyboardactionlisteners.ClipboardActionListener;
 import inc.flide.vim8.keyboardhelpers.InputMethodViewHelper;
+import inc.flide.vim8.preferences.SharedPreferenceHelper;
+import inc.flide.vim8.structures.CustomKeycode;
+import inc.flide.vim8.structures.KeyboardAction;
+import inc.flide.vim8.structures.KeyboardActionType;
+import inc.flide.vim8.utils.ColorsHelper;
+
 import java.text.MessageFormat;
 import java.util.List;
 
-public class ClipboardKeypadView extends ListView {
+public class ClipboardKeypadView extends ConstraintLayoutWithSidebar {
 
     private ClipboardActionListener actionListener;
     private ArrayAdapter<String> adapter;
@@ -34,10 +45,65 @@ public class ClipboardKeypadView extends ListView {
         initialize(context);
     }
 
+
     public void initialize(Context context) {
         actionListener = new ClipboardActionListener((MainInputMethodService) context, this);
+        setupOverallView(context);
+        setupButtonsOnSideBar(actionListener);
+        setColors();
+        setHapticFeedbackEnabled(true);
 
+        SharedPreferenceHelper.getInstance(context).addListener(this::setColors);
+    }
+    private void setupOverallView(Context context) {
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        boolean preferredSidebarLeft = SharedPreferenceHelper
+                .getInstance(context)
+                .getBoolean(
+                        context.getString(R.string.pref_sidebar_left_key),
+                        true);
+
+        if (preferredSidebarLeft) {
+            inflater.inflate(R.layout.clipboard_keypad_left_sidebar_view, this, true);
+        } else {
+            inflater.inflate(R.layout.clipboard_keypad_right_sidebar_view, this, true);
+        }
+        setupClipboardListView();
+    }
+
+    private void setImageButtonTint(int tintColor, int id) {
+        ImageButton button = findViewById(id);
+        button.setColorFilter(tintColor);
+    }
+
+    private void setColors() {
+        Context context = getContext();
+        int backgroundColor =
+                ColorsHelper.getThemeColor(context, R.attr.backgroundColor,
+                        R.string.pref_board_bg_color_key,
+                        R.color.defaultBoardBg);
+        int tintColor =
+                ColorsHelper.getThemeColor(context, R.attr.colorOnBackground,
+                        R.string.pref_board_fg_color_key,
+                        R.color.defaultBoardFg);
+
+        this.setBackgroundColor(backgroundColor);
+        setImageButtonTint(tintColor, R.id.clipboardButton);
+        setImageButtonTint(tintColor, R.id.goToSettingsButton);
+        setImageButtonTint(tintColor, R.id.tabButton);
+        setImageButtonTint(tintColor, R.id.switchToSelectionKeyboard);
+        setImageButtonTint(tintColor, R.id.switchToEmojiKeyboard);
+    }
+
+
+    public void setupClipboardListView() {
+
+        Log.d("clipboard_history", "setup clipboard list view called");
         List<String> clipHistory = actionListener.getClipHistory();
+        ListView clipboardItemsList = this.findViewById(R.id.clipboardItemsList);
+
         adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, (clipHistory)) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -53,19 +119,25 @@ public class ClipboardKeypadView extends ListView {
                 return view;
             }
         };
-        setAdapter(adapter);
+        clipboardItemsList.setAdapter(adapter);
+        Log.d("clipboard_history", "adapter created and set");
 
-        setOnItemClickListener((parent, view, position, id) -> {
+        clipboardItemsList.setOnItemClickListener((parent, itemView, position, id) -> {
             String selectedClip = adapter.getItem(position);
             actionListener.onClipSelected(selectedClip);
         });
     }
 
     public void updateClipHistory() {
+        if (adapter == null) {
+            Log.e("clipboard_history", "adapter seems to be null for no reason");
+        } else {
+
         List<String> clipHistory = actionListener.getClipHistory();
         adapter.clear();
         adapter.addAll(clipHistory);
         adapter.notifyDataSetChanged();
+        }
     }
 
     @Override
