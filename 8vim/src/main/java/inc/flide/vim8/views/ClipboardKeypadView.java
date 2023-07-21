540fd84d7be12b943a1bfa9952cacef088bb5814
Ravi Agarwal <flideravi@gmail.com>
2023-07-21 18:01:38 +0530

    🔥 refactor(ClipboardKeypadView.java): remove unused import statement for android.util.Log The import statement for android.util.Log was removed as it was not being used in the ClipboardKeypadView class. This improves code readability and removes unnecessary clutter.

--- 8vim/src/main/java/inc/flide/vim8/views/ClipboardKeypadView.java
+++ 8vim/src/main/java/inc/flide/vim8/views/ClipboardKeypadView.java
@@ -2,7 +2,6 @@ package inc.flide.vim8.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
