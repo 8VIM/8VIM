e39f9212e443904db11b03c3c7ea34ad56379750
Roberto Guanciale <guancio@gmail.com>
2023-07-29 13:05:41 +0200

    Added option to hide sidebar

--- 8vim/src/main/java/inc/flide/vim8/views/mainkeyboard/MainKeyboardView.java
+++ 8vim/src/main/java/inc/flide/vim8/views/mainkeyboard/MainKeyboardView.java
@@ -3,6 +3,8 @@ package inc.flide.vim8.views.mainkeyboard;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+
 import inc.flide.vim8.MainInputMethodService;
 import inc.flide.vim8.R;
 import inc.flide.vim8.keyboardactionlisteners.MainKeypadActionListener;
@@ -15,6 +17,7 @@ public class MainKeyboardView extends ConstraintLayoutWithSidebar {
 
     private MainKeypadActionListener actionListener;
     private String prefSidebarLeftKey;
+    private String prefSidebarVisibilityKey;
     private SharedPreferenceHelper sharedPreferenceHelper;
     private LayoutInflater inflater;
 
@@ -37,10 +40,13 @@ public class MainKeyboardView extends ConstraintLayoutWithSidebar {
         actionListener = new MainKeypadActionListener((MainInputMethodService) context, this);
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         prefSidebarLeftKey = context.getString(R.string.pref_sidebar_left_key);
+        prefSidebarVisibilityKey = context.getString(R.string.pref_sidebar_visibility_key);
         Theme.getInstance(context).onChange(this::setColors);
         sharedPreferenceHelper = SharedPreferenceHelper
                 .getInstance(context)
-                .addListener(this::initializeView, prefSidebarLeftKey);
+                .addListener(this::initializeView, prefSidebarLeftKey)
+                .addListener(this::initializeView, prefSidebarVisibilityKey);
+
         initializeView();
     }
 
@@ -59,6 +65,13 @@ public class MainKeyboardView extends ConstraintLayoutWithSidebar {
             inflater.inflate(R.layout.main_keyboard_left_sidebar_view, this, true);
         } else {
             inflater.inflate(R.layout.main_keyboard_right_sidebar_view, this, true);
+        }
+        boolean preferredSidebarVisibility = sharedPreferenceHelper.getBoolean(prefSidebarVisibilityKey, true);
+        if (!preferredSidebarVisibility) {
+            View sidebar = this.findViewById(R.id.sidebarButtonsLayout);
+            LayoutParams params = (LayoutParams) sidebar.getLayoutParams();
+            params.horizontalWeight = 0;
+            sidebar.setLayoutParams(params);
         }
     }
 }
