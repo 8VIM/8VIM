1aa086d6cdbc966380ce1868f8bdf443a02dc922
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2023-09-30 07:52:11 +0100

    refactor: 💡 set executionData

--- 8vim/build.gradle.kts
+++ 8vim/build.gradle.kts
@@ -160,6 +160,7 @@ android {
 }
 tasks.withType<JacocoReport> {
     dependsOn(tasks.withType<Test>())
+    executionData.setFrom(project.fileTree(buildDir) { include("**/jacoco/*.exec") })
     reports {
         csv.required.set(false)
     }
